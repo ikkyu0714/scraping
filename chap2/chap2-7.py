2cc581dd6160e4ad8463208df89b572a785095e4
ikkyu0714 <ikkyu1955@gmail.com>
2023-01-25 16:03:36 +0900

    第二弾（２章まで）

---
+++ chap2/chap2-7.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Webページを取得して解析する
+load_url = "https://news.yahoo.co.jp/topics"
+html = requests.get(load_url)
+soup = BeautifulSoup(html.content, "html.parser")
+
+# 全てのliタグを検索して、その文字列を表示する
+chap2 = soup.find(class_="sc-dfVpRl hwKVrp")
+for element in chap2.find_all("li"):
+    print(element.text)
